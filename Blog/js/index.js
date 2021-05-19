efda6dbc7e40aee4ef2d15fcf7a95b8f818d4481
tmhb34 <Anna.Faruga@motorolasolutions.com>
2021-05-19 15:15:09 +0200

    Add DELETE buttons to remove posts - delete particular post by clicking on DELETE button

--- Blog/js/index.js
+++ Blog/js/index.js
@@ -1,5 +1,6 @@
 const posts = [];
 
+//Send values from title, text and author fields
 function send() {
     const title = document.getElementById("post-title").value;
     const text = document.getElementById("post-text").value;
@@ -13,28 +14,40 @@ function Post(title, text, author) {
     this.title = title;
     this.text = text;
     this.author = author;
-    this.getListObject = function() {
+    this.getListObject = function () {
         const li = document.createElement('li');
 
         const h3 = document.createElement('h3');
         const pText = document.createElement('p');
         const pAuthor = document.createElement('p');
+        const button = document.createElement('button');
 
         h3.innerText = this.title;
         pText.innerText = this.text;
         pAuthor.innerText = this.author;
+        button.innerText = "DELETE";
 
         li.appendChild(h3);
         li.appendChild(pText);
         li.appendChild(pAuthor);
+        li.appendChild(button);
+
+        // li.id = "myPost";
+        // button.id = "delete";
+
+        //Delete post by click Delete button
+        button.onclick = function deletePosts() {
+            button.parentElement.remove();
+            console.log("Delete Clicked");
+        };
 
         return li;
     }
 }
 
 function renderPosts(posts) {
-    //clear old lis
-    const list = document.getElementById("post-list")
+    //clear old list
+    const list = document.getElementById("post-list");
     //create a new list
     list.innerHTML = '';
 
